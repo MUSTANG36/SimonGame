08047e253cd8432ba66582dc4ec5b48e39cabc0a
Ricky <ochor01@pfw.edu>
2021-12-16 20:43:55 -0500

    Game.js update
    -Check Answer works
    -

--- game.js
+++ game.js
@@ -3,21 +3,87 @@
 
 //start of game
 
-var levelNum = 0;
+var levelNum = 1;
 var gameOver = false;
-var colorChoices = ["Red","Blue","Yellow","green"];
+var colorChoices = ["red","blue","yellow","green"];
 var gamePattern =[];
 var userClickPattern = [];
+
+
+$(document).keydown(function(e){
+        if(e.key === "a"){
+            randomSequence();
+            $("h1").text("Level "+levelNum);
+            levelNum++;
+        }
+    
+})
 
 //idk how to use the wrong sound?
 //User pick animations
 $(".btn").click(function (e){
-    $(this).fadeOut(250).fadeIn(250);
-    var audio = new Audio("/sounds/" + $(this).attr('id') + ".mp3");
+    
+    $(this).fadeOut(250).fadeIn(250); 
     
     handler(e.target.id);
+    
+    playSound(e.target.id);
+    
+    animatePress(e.target.id);
 
-    switch(e.target.id){
+    checkAnswer(e.target.id);
+})
+
+function animatePress(currentColor){
+
+    $("#"+currentColor).addClass("pressed");
+    
+    setTimeout(() => {  
+        $("#"+currentColor).removeClass("pressed");
+    }, 100)
+
+}
+
+
+function handler(btnColor){
+   var userChoiceColour = btnColor;
+   
+   userClickPattern.push(userChoiceColour);
+    console.log(userClickPattern);
+   
+}
+
+//equality operator will compare the reference for arrays and objeect not the values
+// BAD: userClickPattern === gamePattern
+
+function checkAnswer(currentLevel){
+
+    if(JSON.stringify(userClickPattern) === JSON.stringify(gamePattern) ) {
+       console.log("success"); 
+    }else{
+        console.log("wrong");
+    }
+
+}
+
+
+function randomSequence() {
+
+        var randomNumber = Math.floor(Math.random() * 4);
+        var randomColor = colorChoices[randomNumber];
+
+        gamePattern.push(randomColor);
+        console.log(gamePattern);
+
+
+}
+
+
+function playSound(name){
+
+    var audio = new Audio("/sounds/" + name + ".mp3");
+
+    switch(name){
         case "red":
             audio.play();
             
@@ -38,86 +104,7 @@ $(".btn").click(function (e){
         console.log("swtich ran");
         break;
     }
-})
-
-function handler(btnColor){
-   var userChoiceColour = btnColor;
-   
-   userClickPattern.push(userChoiceColour);
-
-   console.log(userClickPattern);
-}
-
-//when game starts
-function level(){
-    $(document).keydown(function(e){
-    
-        if("Beat level"){
-            if(e.key === "a"){
-                levelNum++;
-                alert("Level " + levelNum);
-                randomSequence(levelNum);
-            
-            }
-        }
-    })
-
-
 }
 
 
 
-
-function randomSequence(numberOfSquares) {
-
-    for(var i=0;i < numberOfSquares; i++){
-        
-
-        var randomNumber = Math.floor(Math.random() * 4);
-        var randomColor = colorChoices[randomNumber];
-
-        gamePattern.push(randomColor);
-       
-
-        switch(randomColor){
-            case "Red":
-                $(".red").delay(180000).toggleClass("pickedSquares");
-            break;
-            case "Yellow":
-                $(".yellow").delay(180000).toggleClass("pickedSquares");
-            break;
-            case "Green":
-                $(".green").delay(1800000).toggleClass("pickedSquares");
-            break;
-            case "Blue":
-                $(".blue").delay(1800000).toggleClass("pickedSquares");
-            break;
-            default: 
-            console.log("swtich ran");
-            break;
-        }
-    }
-
-}
-
-
-function playerPattern(){
-
-    var playerPicks = [];
-
-    var pattern = randomSequence(4);
-   
-}
-
-
-
-
-function checkPattern(){
-
-    var player = playerPattern;
-    var computer = randomSequence;
-
-    console.log("player " + player);
-    console.log("computer " + computer);
-    
-}
